a49a9069a2345df57b0b3e5a759704f8f4ea11ab
kstenschke <info@stenschke.com>
2017-06-26 22:22:38 +0200

    Improved: DOC data type detection
    Signed-off-by: kstenschke <info@stenschke.com>

--- src/com/kstenschke/shifter/utils/UtilsPhp.java
+++ src/com/kstenschke/shifter/utils/UtilsPhp.java
@@ -39,6 +39,34 @@ public class UtilsPhp {
      * @return Name of primitive (PHP) data type
      */
     public static String guessPhpDataTypeByName(String str) {
+        String camelWords[] = UtilsTextual.splitCamelCaseIntoWords(str, true);
+        String lastWord = camelWords[camelWords.length - 1];
+
+        if (   "args".equals(lastWord)
+            || "array".equals(lastWord)
+            || "data".equals(lastWord)
+            || "ids".equals(lastWord)
+            || "items".equals(lastWord)
+            || "list".equals(lastWord)
+            || "pieces".equals(lastWord)
+            || "params".equals(lastWord)
+            || "parameters".equals(lastWord)
+            || "values".equals(lastWord)
+        ) {
+            return "array";
+        }
+
+        if (   "do".equals(camelWords[0])
+            || "get".equals(camelWords[0])
+            || "has".equals(camelWords[0])
+            || "is".equals(camelWords[0])
+            || "return".equals(camelWords[0])
+            || "should".equals(camelWords[0])
+            || "with".equals(camelWords[0]) || "without".equals(camelWords[0])
+        ) {
+            return "bool";
+        }
+
         str = str.toLowerCase();
 
         if (str.matches("\\w*name|\\w*title|\\w*url")) {
@@ -46,9 +74,6 @@ public class UtilsPhp {
         }
         if (str.matches("(\\w*delim(iter)*|\\w*dir(ectory)*|\\w*domain|filename\\w*|\\w*key|\\w*link|\\w*name|\\w*path\\w*|\\w*prefix|\\w*suffix|charlist|comment|\\w*file(name)*|format|glue|haystack|html|intput|locale|message|name|needle|output|replace(ment)*|salt|separator|str(ing)*|url)\\d*")) {
             return "string";
-        }
-        if (str.matches("(arr(ay)|\\w*pieces|\\w*list|\\w*items|\\w*ids)\\d*")) {
-            return "array";
         }
         if (str.matches("(\\w*day|\\w*end|\\w*expire|\\w*handle|\\w*height|\\w*hour(s)*|\\w*id|\\w*index|\\w*len(gth)*|\\w*mask|\\w*pointer|\\w*quality|\\w*s(e)*ize|\\w*start|\\w*step(s)*|tick|\\w*year\\w*|ascii|base|blue|ch|chunklen|fp|green|len|limit|\\w*max|\\w*min|\\w*mode|month|\\w*multiplier|now|num|offset|\\w*op(eration)*|\\w*pos(ition)*|red|\\w*time(stamp)*|week|\\w*wid(th)*|x|y)\\d*")) {
             return "int";
@@ -59,11 +84,8 @@ public class UtilsPhp {
         if (str.matches("(\\wmodel|\\w*obj(ect)*)\\d*")) {
             return "Object";
         }
-        if (str.matches("(\\w*s)\\d*|\\w*arr(ay)*|\\w*items|\\w*data|data\\w*")) {
+        if (str.matches("(\\w*s)\\d*")) {
             return "array";
-        }
-        if (str.matches("(do\\w*|has\\w+|is\\w+|return\\w*|should\\w*)")) {
-            return "bool";
         }
 
         return "unknown";
