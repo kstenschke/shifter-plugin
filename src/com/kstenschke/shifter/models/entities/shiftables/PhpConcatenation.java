214770ce14d287208d9b7550726b7317592c03c0
kstenschke <info@stenschke.com>
2019-04-25 00:32:20 +0200

    simplify phpConcatenation: reduce nested logic levels

--- src/com/kstenschke/shifter/models/entities/shiftables/PhpConcatenation.java
+++ src/com/kstenschke/shifter/models/entities/shiftables/PhpConcatenation.java
@@ -17,9 +17,7 @@ package com.kstenschke.shifter.models.entities.shiftables;
 
 import com.kstenschke.shifter.utils.UtilsTextual;
 
-/**
- * PHP Variable (word w/ $ prefix)
- */
+// PHP Variable (word w/ $ prefix)
 public class PhpConcatenation {
 
     private boolean isPhpConcatenation = false;
@@ -30,25 +28,24 @@ public class PhpConcatenation {
     private String partLHS = null;
     private String partRHS = null;
 
-    /**
-     * Constructor
-     */
+    // Constructor
     public PhpConcatenation(String str) {
         String strJoined = UtilsTextual.removeLineBreaks(str.trim());
+        if (strJoined.length() <= 4 || !strJoined.contains(".")) return;
 
-        if (strJoined.length() > 4 && strJoined.contains(".")) {
-            extractParts(strJoined);
-            if (null != partLHS && null != partRHS) {
-                isPhpConcatenation = true;
-                if (null != offsetDot) {
-                    char charBeforeDot = strJoined.charAt(offsetDot - 1);
-                    char charAfterDot = strJoined.charAt(offsetDot + 1);
-                    if ((' ' == charBeforeDot && ' ' == charAfterDot) || (charBeforeDot == '\t' && charAfterDot == '\t')) {
-                        isDotWhitespaceWrapped = true;
-                        dotWrapChar = String.valueOf(charAfterDot);
-                    }
-                }
-            }
+        extractParts(strJoined);
+        if (null == partLHS || null == partRHS) return;
+
+        isPhpConcatenation = true;
+        if (null == offsetDot) return;
+
+        char charBeforeDot = strJoined.charAt(offsetDot - 1);
+        char charAfterDot = strJoined.charAt(offsetDot + 1);
+        if ((' ' == charBeforeDot && ' ' == charAfterDot) ||
+                (charBeforeDot == '\t' && charAfterDot == '\t')
+        ) {
+            isDotWhitespaceWrapped = true;
+            dotWrapChar = String.valueOf(charAfterDot);
         }
     }
 
@@ -68,92 +65,89 @@ public class PhpConcatenation {
         String strTrimmed = str.trim();
         Integer strLen    = strTrimmed.length();
 
-        if (strLen > 4) {
-            // Detect LHS Type / how it ends
-            StringBuilder leftHandSide = new StringBuilder(strTrimmed.substring(0, 1));
-            String endChar = detectConcatenationTypeAndGetEndingChar(leftHandSide.toString(), false);
+        if (strLen <= 4) return;
 
-            if (null != endChar) {
-                Integer currentOffset = 1;
-                String currentChar;
-                boolean isFailed = false;
+        // Detect LHS Type / how it ends
+        StringBuilder leftHandSide = new StringBuilder(strTrimmed.substring(0, 1));
+        String endChar = detectConcatenationTypeAndGetEndingChar(leftHandSide.toString(), false);
+        if (null == endChar) return;
 
-                // Iterate until the end of the LHS part (denoted by ending character of detected operand type)
-                boolean isFoundEndOfLHS = false;
-                boolean isFoundDot = false;
+        Integer currentOffset = 1;
+        String currentChar;
 
-                while (currentOffset < strLen && !isFoundEndOfLHS) {
-                    currentChar    = strTrimmed.substring(currentOffset, currentOffset+1);
-                    if (currentChar.equals(endChar)) {
-                        // Ignore escaped end-char
-                        if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset-1, currentOffset))) {
-                            isFoundEndOfLHS = true;
-                            if (".".equals(endChar)) {
-                                isFoundDot = true;
-                                offsetDot    = currentOffset;
-                            } else {
-                                leftHandSide.append(currentChar);
-                            }
-                        }
+        // Iterate until the end of the LHS part (denoted by ending character of detected operand type)
+        boolean isFoundEndOfLHS = false;
+        boolean isFoundDot = false;
+
+        while (currentOffset < strLen && !isFoundEndOfLHS) {
+            currentChar = strTrimmed.substring(currentOffset, currentOffset + 1);
+            if (currentChar.equals(endChar)) {
+                // Ignore escaped end-char
+                if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset - 1, currentOffset))) {
+                    isFoundEndOfLHS = true;
+                    if (".".equals(endChar)) {
+                        isFoundDot = true;
+                        offsetDot = currentOffset;
                     } else {
                         leftHandSide.append(currentChar);
                     }
+                }
+            } else {
+                leftHandSide.append(currentChar);
+            }
+            currentOffset++;
+        }
+
+        // Iterate until dot found, abort search if illegal (=other than dot or white-space) character found
+        while (currentOffset < strLen && !isFoundDot) {
+            currentChar = strTrimmed.substring(currentOffset, currentOffset + 1);
+            if (".".equals(currentChar)) {
+                if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset - 1, currentOffset))) {
+                    isFoundDot = true;
+                    offsetDot = currentOffset;
                     currentOffset++;
                 }
-                // Iterate until dot found, abort search if illegal (=other than dot or white-space) character found
-                while (currentOffset < strLen && !isFoundDot && !isFailed) {
-                    currentChar = strTrimmed.substring(currentOffset, currentOffset+1);
-                    if (".".equals(currentChar)) {
-                        if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset-1, currentOffset))) {
-                            isFoundDot = true;
-                            offsetDot = currentOffset;
-                            currentOffset++;
-                        }
-                    } else if (" ".equals(currentChar) || "\t".equals(currentChar)) {
-                        currentOffset++;
-                    } else {
-                        isFailed    = true;
+            } else if (" ".equals(currentChar) || "\t".equals(currentChar)) {
+                currentOffset++;
+            } else return;
+        }
+        if (!isFoundDot) return;
+
+        // Look for RHS part
+        strTrimmed = strTrimmed.substring(currentOffset).trim();
+        strLen = strTrimmed.length();
+        currentOffset = 0;
+
+        String rightHandSide = strTrimmed.substring(currentOffset, currentOffset + 1);
+
+        endChar = detectConcatenationTypeAndGetEndingChar(rightHandSide, true);
+        boolean isFailed = false;
+        if (null == endChar) return;
+
+        currentOffset += 1;
+        boolean isFoundEndOfRHS = false;
+        while (currentOffset < strLen && !isFoundEndOfRHS) {
+            currentChar = strTrimmed.substring(currentOffset, currentOffset + 1);
+            if (currentChar.equals(endChar) || ("".equals(endChar) && currentOffset + 1 == strLen)) {
+                // Ignore escaped end-char
+                if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset - 1, currentOffset))) {
+                    isFoundEndOfRHS = true;
+                    rightHandSide += currentChar;
+                    currentOffset++;
+
+                    if (strLen > currentOffset && strTrimmed.substring(currentOffset).trim().length() == 0) {
+                        // If concatenation ends at current offset, the string should as well
+                        isFailed = true;
+                    }
+
+                    if (!isFailed) {
+                        partLHS = leftHandSide.toString();
+                        partRHS = rightHandSide;
                     }
                 }
-                // Look for RHS part
-                if (!isFailed && isFoundDot) {
-                    strTrimmed = strTrimmed.substring(currentOffset).trim();
-                    strLen = strTrimmed.length();
-                    currentOffset = 0;
-
-                    String rightHandSide = strTrimmed.substring(currentOffset, currentOffset+1);
-
-                    endChar    = detectConcatenationTypeAndGetEndingChar(rightHandSide, true);
-                    if (null != endChar) {
-                        currentOffset+=1;
-                        boolean isFoundEndOfRHS = false;
-
-                        while (currentOffset < strLen && !isFoundEndOfRHS) {
-                            currentChar    = strTrimmed.substring(currentOffset, currentOffset+1);
-                            if (currentChar.equals(endChar) || ("".equals(endChar) && currentOffset + 1 == strLen)) {
-                                // Ignore escaped end-char
-                                if (currentOffset > 0 && !"\\".equals(strTrimmed.substring(currentOffset-1, currentOffset))) {
-                                    isFoundEndOfRHS = true;
-                                    rightHandSide += currentChar;
-                                    currentOffset++;
-
-                                    if (strLen > currentOffset && strTrimmed.substring(currentOffset).trim().length() == 0) {
-                                        // If concatenation ends at current offset, the string should as well
-                                        isFailed = true;
-                                    }
-
-                                    if (!isFailed) {
-                                        partLHS    = leftHandSide.toString();
-                                        partRHS    = rightHandSide;
-                                    }
-                                }
-                            } else {
-                                rightHandSide += currentChar;
-                                currentOffset++;
-                            }
-                        }
-                    }
-                }
+            } else {
+                rightHandSide += currentChar;
+                currentOffset++;
             }
         }
     }
@@ -164,18 +158,14 @@ public class PhpConcatenation {
      * @return String   Ending character    . / " / ' / empty char (=ending is end of string) / null (=no type detected)
      */
     private String detectConcatenationTypeAndGetEndingChar(String str, boolean isRHS) {
-        if ("$".equals(str)) {
-            // Identified part as PHP Variable
-            return isRHS ? "" : ".";
-        }
-        if ("\"".equals(str)) {
-            // Identified parts as string wrapped within double quotes
-            return "\"";
-        }
-        if ("\'".equals(str)) {
-            // Identified parts as string wrapped within single quotes
-            return "\'";
-        }
+        // Part is PHP Variable?
+        if ("$".equals(str)) return isRHS ? "" : ".";
+
+        // Parts are string wrapped within double quotes?
+        if ("\"".equals(str)) return "\"";
+
+        // Parts are string wrapped within single quotes?
+        if ("\'".equals(str)) return "\'";
 
         return null;
     }
